0bdf2b4e79ec5b2637dc89835628931a67fcbc92
alexmiloeski <a.miloeski@gmail.com>
2024-07-08 10:46:06 +0200

    add cross-origin on all origins

--- backend/demo/src/main/java/com/example/demo/user/UserController.java
+++ backend/demo/src/main/java/com/example/demo/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.user;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
 
     @GetMapping
